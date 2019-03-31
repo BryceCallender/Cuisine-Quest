f971a9d7a6c4129388ff314613cb853df9396896
Bryce Callender <brycec848@gmail.com>
2019-03-30 18:13:19 -0700

    Beginning of Save File logic with quests

---
+++ Cuisine Quest/Assets/Scripts/Quest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum QuestType
+{
+    Kill,
+    Collect
+}
+
+[System.Serializable]
+public class Quest : ISaveable 
+{
+    [SerializeField]
+    private string questName;
+    [SerializeField]
+    private QuestType questType;
+    [SerializeField]
+    //TODO::Change the string to Required Item Class to show amount 
+    //and which gameobject they will need to get
+    private List<RequiredItem> requiredItems;
+    [SerializeField]
+    private bool completedQuest;
+
+    public string Save()
+    {
+        return JsonUtility.ToJson(this,true);
+    }
+
+    bool checkQuestCompletion()
+    {
+        return true;
+    }
+
+}
---
+++ Cuisine Quest/Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveSystem : MonoBehaviour
+{
+    public List<GameObject> saveableObjects;
+    public List<Quest> quests;
+    private StreamWriter saveFileWriter;
+    private StreamReader saveFileReader;
+    private string path;
+
+    private void Start()
+    {
+        path = Path.Combine(Application.persistentDataPath, "CuisineQuest.save");
+        //Opens the stream writer to the file path and Path.combine
+        //guarentees that we will have the right path written by the OS
+        saveFileWriter = new StreamWriter(path);
+        //TODO::Remove this and put elsewhere this is just for testing
+        SaveGame();
+        //LoadGame();
+    }
+
+    /*
+     * Saves the game going thru the list of saveable objects and calling 
+     * the save function which will return the json format to the object
+     * to write to the file. Once the file is saved the writer will close
+     * thus flushing the buffer into the file correctly.   
+     */
+    public void SaveGame()
+    {
+        //foreach(GameObject saveObject in saveableObjects)
+        //{
+        //    print("Saved File");
+        //    saveFileWriter.Write(saveObject.GetComponent<ISaveable>().Save());
+        //}
+
+        foreach (Quest quest in quests)
+        {
+            print("Saved File");
+            saveFileWriter.Write(quest.Save());
+        }
+
+        saveFileWriter.Close();
+    }
+
+    public void LoadGame()
+    {
+        saveFileReader = new StreamReader(path);
+        JsonUtility.FromJson<Quest>(saveFileReader.ReadToEnd());
+    }
+}
