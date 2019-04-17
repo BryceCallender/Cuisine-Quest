291fa7e41803f0ca7400fec3c034c42c84219ec3
thinhman <thinhnguyen.nguyen2353@gmail.com>
2019-04-16 20:46:42 -0700

    Animation

--- Cuisine Quest/Assets/Scripts/PlayerController.cs
+++ Cuisine Quest/Assets/Scripts/PlayerController.cs
@@ -8,10 +8,13 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     private Animator anim;
     private bool playerMoving;
-    private Vector2 lastMove; 
+    private Vector2 lastMove;
+    private float H_Axis;
+    private float V_Axis;
+
 
     // Use this for initialization
-	void Start ()
+    void Start ()
     {
         anim = GetComponent<Animator>();
 	}
@@ -19,24 +22,25 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-
+        H_Axis = Input.GetAxisRaw("Horizontal");
+        V_Axis = Input.GetAxisRaw("Vertical");
         playerMoving = false;
 
-		if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+		if(H_Axis > 0.5f || H_Axis < -0.5f)
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
             playerMoving = true;
             lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
 
-        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        if (V_Axis > 0.5f || V_Axis < -0.5f)
         {
             transform.Translate(new Vector3( 0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
             playerMoving = true;
             lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
-        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
-        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetFloat("MoveX", H_Axis);
+        anim.SetFloat("MoveY", V_Axis);
         anim.SetBool("PlayerMoving", playerMoving);
         anim.SetFloat("LastMoveX", lastMove.x);
         anim.SetFloat("LastMoveY", lastMove.y);
